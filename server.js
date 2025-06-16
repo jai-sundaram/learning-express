dc99434be6b6e9357f211a0b6f9f965664936e56
jaivatsan-elango-sundaram <je.sundaram@gmail.com>
2025-06-16 19:00:22 -0400

    started w express

---
+++ server.js
@@ -0,0 +1,36 @@
+//create the express server
+const express = require('express');
+//create an application
+const app = express();
+//were going to make it listen on port 3000
+app.listen(3003)
+
+
+//basic routing
+
+//making a get request
+//first parameter will be the path
+//second parameter will be a function that takes 2 different parameters
+//first paramter of the function is the request, second parameter is the response
+app.get('/', (req, res) => {
+        //this code below will be eexecuted
+        console.log("hello")
+        //we can set the response
+        //the send method will send out whatever we put in the method
+       // res.send("Hi")
+
+        //we could send a status code
+        //res.sendStatus(500);
+        //we can also send a message with the status code
+        //res.status(500).send('error')
+        //could send a json
+        res.json({message: "Error"})
+        //can also send in a file we want a file to download
+        //inside the function put to the path to the file
+        res.download("server.js")
+
+    }
+
+
+
+)
